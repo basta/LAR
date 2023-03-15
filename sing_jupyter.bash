cf61d3a6fedbd00f6d574596102dbd5f62ad5dc6
basta <jsembasta@gmail.com>
2023-03-15 17:32:18 +0100

    added launch scripts

---
+++ sing_jupyter.bash
@@ -0,0 +1,2 @@
+source /opt/ros/lar/setup.bash
+~/.local/bin/jupyter-notebook --NotebookApp.token='' --NotebookApp.password=''
---
+++ sing_rob.bash
@@ -0,0 +1,2 @@
+source /opt/ros/lar/setup.bash
+roslaunch robolab_turtlebot bringup_realsense_D435.launch
