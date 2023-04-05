80818ebb90f1983614203bc68adab728a1bbfe45
Ondrej Bastar <bastaond@bullseye>
2023-04-05 17:37:21 +0200

    blaa

---
+++ loggingSetup.py
@@ -0,0 +1,7 @@
+import logging
+logger = logging.getLogger("robot")
+logger.setLevel(logging.DEBUG)
+handle = logging.FileHandler("robot.log")
+handle.setLevel(logging.DEBUG)
+logger.addHandler(handle)
+handle.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
